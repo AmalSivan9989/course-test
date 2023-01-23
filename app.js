f201adaeeb907754f8f696187660e53ced2789bd
Amal Sivan <amalcoursera-something.com>
2023-01-23 23:13:19 +0530

    vv

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-document.write("hello");
+( function (){
+  angular.module("convertapp",[])
+  .controller("appcontroller",appcontroller)
+  appcontroller.$inject=($scope,$filter)
+function appcontroller($scope,$filter){
+  $scope.name="amal"
+
+
+$scope.message=function (){
+  var msg="amal is a good boy"
+  var output=$filter("uppercase")(msg)
+  return output;
+}};
+
+
+
+
+
+
+
+})();
